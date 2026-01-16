2978d0f97147a33fa52307470f06e5355efc8da8
Charles Watari <cgwatari@asu.edu>
2026-01-16 14:55:52 -0800

    Resolved merge conflict between featureA and featureB.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main { 
 	public static void main(String[] args) { 
 
-		System.out.println("Welcome to Git Practice! Features 1 and B merged"); 
+		System.out.println("Welcome to Git Practice! Features A and B merged"); 
 		greet("FeatureA_User"); 
 		greet("FeatureB_User"); }
 
