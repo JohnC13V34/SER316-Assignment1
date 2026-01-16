a79b74fcbc953b4ea4a3b9086669d3775cc41d09
Charles Watari <cgwatari@asu.edu>
2026-01-16 14:34:28 -0800

    Feature A changes: modified greeting in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main { 
 	public static void main(String[] args) { 
-		System.out.println("Welcome to Git Practice!"); 
-		greet("Student"); } 
+		System.out.println("Welcome to Git Practice! This is feature A"); 
+		greet("FeatureA_User"); } 
 	
 	public static void greet(String name) { 
 		System.out.println("Hello, " + name + "!"); }
